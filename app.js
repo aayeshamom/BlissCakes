5c4fb0251c2946aa537a8036d0fa1eb4f8d581c3
Aayesha <aayeshamomin082@gmail.com>
2024-07-08 12:20:20 +0530

    updations

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Listing = require("./models/listing.js");
 
-main().catch(err => console.log(err));
+main().
+then(() =>{
+    console.log("connected to db");})
+    .catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/BilssCakes");
